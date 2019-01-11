74bdfd41695131f2af99701debf57418e5ab009b
Luther <luther0211@hotmail.com>
2019-01-10 21:53:53 -0600

    Added search & display results functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,35 @@
 import Search from './models/Search'
+import * as searchView from './views/searchView'
+import { elements } from './views/base'
 
 // Global State Of The App
 //*- Search object
 //*- Current recipe object
 //*- Shopping list object
 //*- Liked recipes object
-const state = {
-  
-}
+const state = {}
 
 const controlSearch = async () => {
   // 1 Get query from view
-  const query = "pizza" //TO-DO
+  const query = searchView.getInput();
 
   if(query) {
     //2 New Search obj & add it to state
     state.search = new Search(query);
 
     //3 Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
 
     //4 search for recipes
     await state.search.getResult();
 
     //5 Render results on UI
-    console.log(state.search.result)
+    searchView.renderResults(state.search.result)
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -9,9 +9,9 @@ export default class Search {
   async getResult(){ // async METHOD
     const key = "b9cc1ecc4f3fd0a765e79e0343dc57da";
     try{
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this. query}`)
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
         this.result = res.data.recipes
-        //console.log(this.result);
+        console.log(this.result);
     } catch(error) {
         alert(error)
     }
