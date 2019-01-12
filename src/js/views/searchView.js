4c170004ab5a651db68c815166ff87ead4534136
Luther <luther0211@hotmail.com>
2019-01-11 19:32:13 -0600

    added loader animation

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -26,7 +26,7 @@ const limitRecipeTitle = (title, limit=17) => {
 
 const renderRecipe = recipe => {
   const markup = `
-  <li>
+  <li class="resultsList" title="${recipe.title}">
     <a class="results__link" href="#${recipe.recipe_id}">
         <figure class="results__fig">
             <img src="${recipe.image_url}" alt="${recipe.title}">
