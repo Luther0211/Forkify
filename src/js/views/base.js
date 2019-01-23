d0d5f2d1282bc610845ed7b86fe88bf84b3a9203
Luther <luther0211@hotmail.com>
2019-01-22 21:41:43 -0600

    Created the shopping List view

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,11 +1,12 @@
 export const elements = {
-  logo: document.querySelector('.header__logo'),
-  searchForm: document.querySelector('.search'),
-  searchInput: document.querySelector('.search__field'),
-  searchResList: document.querySelector('.results__list'),
-  searchRes: document.querySelector(".results"),
-  searchResPages: document.querySelector('.results__pages'),
-  recipe : document.querySelector('.recipe')
+  logo:             document.querySelector('.header__logo'),
+  searchForm:       document.querySelector('.search'),
+  searchInput:      document.querySelector('.search__field'),
+  searchResList:    document.querySelector('.results__list'),
+  searchRes:        document.querySelector(".results"),
+  searchResPages:   document.querySelector('.results__pages'),
+  recipe:           document.querySelector('.recipe'),
+  shopping:         document.querySelector('.shopping__list')
 };
 
 export const elementStrings = {
